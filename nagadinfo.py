89b0534e20583e90a79586b1bff20827c9d0a916
BDTermuxHacker <130853131+BDTermuxHacker12@users.noreply.github.com>
2023-12-10 01:02:58 +0600

    Create nagadinfo.py

---
+++ nagadinfo.py
@@ -0,0 +1,38 @@
+RED = "\33[91m"
+BLUE = "\33[94m"
+GREEN = "\033[32m"
+YELLOW = "\033[93m"
+PURPLE = '\033[0;35m' 
+CYAN = "\033[36m"
+END = "\033[0m"
+
+banner = f"""
+\033[32m██████╗ ████████╗██╗  ██╗
+\033[32m██╔══██╗╚══██╔══╝██║  ██║
+\033[32m██████╔╝   ██║   ███████║
+\033[32m██╔══██╗   ██║   ██╔══██║
+\033[32m██████╔╝   ██║   ██║  ██║
+\033[32m╚═════╝    ╚═╝   ╚═╝  ╚═╝"""
+
+print(banner)
+
+import requests
+
+#Telegram channel :- https://t.me/BDTermuxHacker
+
+url = "https://app.mynagad.com:20002/api/user/check-user-status-for-log-in"
+number = input("Enter phone number: ")  # Assuming you get the phone number as input
+
+headers = {
+    "X-KM-User-AspId": "100012345612345",
+    "X-KM-User-Agent": "ANDROID/1152",
+    "X-KM-DEVICE-FGP": "19DC58E052A91F5B2EB59399AABB2B898CA68CFE780878C0DB69EAAB0553C3C6",
+    "X-KM-Accept-language": "bn",
+    "X-KM-AppCode": "01",
+}
+
+params = {"msisdn": number}
+
+response = requests.get(url, headers=headers, params=params)
+
+print(response.text)
